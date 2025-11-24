13527daaa85c37e1da26f5fd8926aafd2ad39075
Vedantlights <sneha@vedantlights.com>
2025-11-24 13:52:37 +0530

    Update App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/search" element={<h1>Search Page</h1>} />
+        <Route path="/agents" element={<h1>Agents Page</h1>} />
+        <Route path="/contact" element={<h1>Contact Page</h1>} />
+        <Route path="/login" element={<h1>Login Page</h1>} />
+        <Route path="/register" element={<h1>Register Page</h1>} />
+        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
